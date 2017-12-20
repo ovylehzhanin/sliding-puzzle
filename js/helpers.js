e12cfb12d1c7d2a809244454e5ca62bfcedd32c7
Alexey Vylegzhanin <a.a.vylegzhanin@gmail.com>
2017-12-20 22:55:09 +0200

    initial changes

---
+++ js/helpers.js
@@ -0,0 +1,12 @@
+/*
+ * Helpers-------------------------
+*/
+function _qs(selector, scope) {
+  return (scope || document).querySelector(selector);
+}
+
+function _qsa(selector, scope) {
+  return (scope || document).querySelector(selector);
+}
+/*---------------------------------*/
+
--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// ha-ha-ha...
+function main() {
+  let root = _qs('#root'),
+    fragment = document.createDocumentFragment(),
+    testMatrix = [
+      [1, 2, 3, 4],
+      [5, 6, 7, 8].
+      [9, 10, 11, 12].
+      [13, 14, 15, 16].
+    ];
+
+  console.log(root);
+}
+
+window.addEventListener('DOMContentLoaded', main, false);

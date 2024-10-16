b282849c08c29235b047977dc44ff85dba7c84ed
ogdev <notomeo@proton.me>
2024-10-16 12:58:20 +0300

    new direction constants

--- src/constants.js
+++ src/constants.js
@@ -15,3 +15,22 @@ export const KEY = {
   RIGHT: 39,
   DOWN: 40
 };
+
+const ARROW_LEFT = 'ArrowLeft';
+const ARROW_UP = 'ArrowUp';
+const ARROW_RIGHT = 'ArrowRight';
+const ARROW_DOWN = 'ArrowDown';
+
+export const DIR_LEFT = 'left';
+export const DIR_UP = 'up';
+export const DIR_RIGHT = 'right';
+export const DIR_DOWN = 'down';
+
+export const ARROWS_KEYS = [ARROW_LEFT, ARROW_UP, ARROW_RIGHT, ARROW_DOWN];
+
+export const DIRECTION_DICT = {
+  [ARROW_LEFT]: DIR_RIGHT,
+  [ARROW_UP]: DIR_DOWN,
+  [ARROW_RIGHT]: DIR_LEFT,
+  [ARROW_DOWN]: DIR_UP,
+};

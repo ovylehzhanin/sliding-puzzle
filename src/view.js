c68f133befc41dc18b271d14cfd06d7210f1a75e
ogdev <notomeo@proton.me>
2024-10-16 14:24:07 +0300

    view adjustments

--- src/view.js
+++ src/view.js
@@ -1,130 +1,48 @@
-// import { Observer } from './observer';
-import {ARROWS_KEYS, DIRECTION_DICT, KEY} from './constants';
-import {_h} from './helpers';
+import { ARROWS_KEYS, DIRECTION_DICT } from './constants';
 
 export default class View {
-  constructor() {
-    this.elements = null; 
+
+  get $$rootEl() {
+    return document.querySelector('#root');
   }
 
-  _cacheTheDom() {
-    let elements = {
-      root: _h.qs('#root'),
-      body: _h.qs('body'),
-      statistic: _h.qs('#statistic', this.body),
-      shuffleButton: _h.qs('#makeShuffle', this.body)
-    }; 
-
-    this.elements = elements;
+  get $$bodyEl() {
+    return document.querySelector('body');
   }
 
-  _createItemNode(currentRow, currentColumn, innerValue) {
-    let self = this,
-      itemNode = _h.ce('div');
-
-    itemNode.dataset.row = currentRow;
-    itemNode.dataset.column = currentColumn;
-    itemNode.dataset.value = innerValue;
-    itemNode.dataset.clickDirection = '';
-    itemNode.className = 'item';
-    itemNode.addEventListener('click', event => {self._handlerItemClick(itemNode)}, false);
-    
-    return itemNode; 
+  get $$statsEl() {
+    return document.querySelector('#statistic');
   }
 
-  _handlerItemClick(itemNode) {
-    if (itemNode.dataset.clickDirection != '') {
-      let direction = itemNode.dataset.clickDirection; 
-
-      Observer.callTrigger('itemClicked', direction, null);
-    }
+  get $$shuffleBttn() {
+    return document.querySelector('#makeShuffle');
   }
 
-  _getDirectionFromKeyCode(keyCode) {
-    switch (keyCode) {
-      case KEY.LEFT:
-        return 'right';
-        break;
-
-      case KEY.UP:
-        return 'down';
-        break;
-
-      case KEY.RIGHT:
-        return 'left';
-        break;
-
-      case KEY.DOWN:
-        return 'up';
-        break;
-
-      default:
-        break;
-    }
-  }
+  constructor() { }
 
   renderItems(items, possibleMoves) {
-    let self = this,
-      _fragment = _h.cdf(),
-      {root} = this.elements;
+    const _fragment = document.createDocumentFragment();
     
     for (let row = 0, rows = items.length; row < rows; row += 1) {
       for (let column = 0, columns = items[row].length; column < columns; column += 1) {
         if (items[row][column] != ' ') {
-          _fragment.appendChild( self._createItemNode(row, column, items[row][column]) );
+          _fragment.appendChild(this._createItemNode(row, column, items[row][column]));
         }
       }
     }
 
-    root.innerHTML = '';
-    root.appendChild( _fragment );
-    self._highlightPossibleMoves(possibleMoves);
+    this.$$rootEl.innerHTML = '';
+    this.$$rootEl.appendChild(_fragment);
+    this._highlightPossibleMoves(possibleMoves);
   }
 
   renderStatistic(movesCount) {
-    let movesCountNode = _h.qs('.statistic__moves-value');
-
-    movesCountNode.textContent = movesCount;
+    this.$$statsEl.querySelector('.statistic__moves-value').textContent = movesCount;
   };
 
-  generateMovesArray(count) {
-    let self = this,
-      movesArray = [];
-
-    for (let i = 0; i < count; i += 1) {
-      movesArray.push( self._getDirectionFromKeyCode(generateKeyCode(KEY.LEFT, KEY.DOWN + 1)) );
-    }
-    
-    function generateKeyCode(min, max) {
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
-
-    return movesArray;
-  }
-
-  _highlightPossibleMoves(possibleMoves) {
-    this.elements.root.childNodes.forEach(tile => {
-      tile.classList.remove('highlighted'); 
-      tile.dataset.clickDirection = '';
-    });
-
-    for (let move in possibleMoves) {
-      const tileToHighlight = _h.qs(this._getTileSelector(possibleMoves[move]), root);
-
-      if (tileToHighlight) {
-        tileToHighlight.classList.add('highlighted');
-        tileToHighlight.dataset.clickDirection = move;
-      }
-    } 
-  }
-
-  _getTileSelector([row, col]) {
-    return `.item[data-row="${row}"][data-column="${col}"]`;
-  }
-
   moveTile(currentPosition, newPosition, possibleMoves) {
-    const elementToMove = _h.qs(this._getTileSelector(newPosition), this.elements.root);
-    
+    const elementToMove = this.$$rootEl.querySelector(this._getTileSelector(newPosition));
+
     if (elementToMove) {
       elementToMove.dataset.row = currentPosition[0];
       elementToMove.dataset.column = currentPosition[1];
@@ -139,9 +57,42 @@ export default class View {
         onArrowKeyPress(DIRECTION_DICT[event.code]);
       }
     });
+    this.$$rootEl.addEventListener('click', (event) => {
+      if (!!event.target?.dataset?.clickDirection) {
+        onItemClick(event.target?.dataset?.clickDirection);
+      }
+    });
   }
-  
-  init() {
-    this._cacheTheDom();
-  };
+
+  _createItemNode(row, col, innerValue) {
+    const itemNode = document.createElement('div');
+
+    itemNode.dataset.row = row;
+    itemNode.dataset.column = col;
+    itemNode.dataset.value = innerValue;
+    itemNode.dataset.clickDirection = '';
+    itemNode.className = 'tile';
+    
+    return itemNode; 
+  }
+
+  _highlightPossibleMoves(possibleMoves) {
+    this.$$rootEl.childNodes.forEach(tile => {
+      tile.classList.remove('highlighted'); 
+      tile.dataset.clickDirection = '';
+    });
+
+    for (const move in possibleMoves) {
+      const tileToHighlight = this.$$rootEl.querySelector(this._getTileSelector(possibleMoves[move]));
+
+      if (tileToHighlight) {
+        tileToHighlight.classList.add('highlighted');
+        tileToHighlight.dataset.clickDirection = move;
+      }
+    } 
+  }
+
+  _getTileSelector([row, col]) {
+    return `.tile[data-row="${row}"][data-column="${col}"]`;
+  }
 }
